1c9f54c94ec6cf9786d573cd351001ec7081401a
Jinhan <wlsgks846@gmail.com>
2023-09-02 22:38:19 +0900

    Navigation edit

--- my-app/src/components/Menu.tsx
+++ my-app/src/components/Menu.tsx
@@ -1,21 +1,9 @@
 import DrinkList from './DrinkList';
 import React from 'react';
-import productList from '@/pages/productList.json'
+import products from '@/pages/productList.json'
 
 const MenuButton = () => {
-  const buttonStyle = {
-    width: '110px',
-    height: '110px',
-    borderRadius: '10px',
-  };
-
   return (
-    <>
-    <div className='grid gap-x-23 gap-y-59 grid-cols-3'>
-    <button style={buttonStyle}>
-
-    </button>
-    </div>
-    </>
+    <button class='w-110 h-110 bg-Pink-50'></button>
   )
 }
--- my-app/src/components/Navigation.tsx
+++ my-app/src/components/Navigation.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
 
 const Navigation: React.FC = () => {
-  const buttonStyle = {
-    marginTop: '74px',
-    marginLeft: '5px',
-    width: '120px',
-    height: '40px',
-    fontFamily: 'Inter',
-    fontSize: '14px',
-    fontStyle: 'normal',
-    fontWeigth: '500px',
-    lineHeight: '24px',
-    fontColor: '#000',
-    backgroundColor: '#FFF1F2',
-  };
-
   return (
-    <div className='grid gap-x-4 grid-cols-3'>
-      <button style={buttonStyle}>시즌 메뉴</button>
-      <button style={buttonStyle}>커피</button>
-      <button style={buttonStyle}>디저트</button>
-    </div>
-  );
-}
+    <button className='w-120 h-40 bg-#FFF1F2 text-black text-sm grid gap-x-4 grid-cols-3'>
+      <div>시즌 메뉴</div>
+      <div>커피</div>
+      <div>디저트</div>
+    </button>
+  )
+};
 
 export default Navigation;
