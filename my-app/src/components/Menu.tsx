0ba3fe6f335e1b9c11079f5533593fbc5002a210
Jinhan <wlsgks846@gmail.com>
2023-09-08 04:14:44 +0900

    first page

--- my-app/src/components/Menu.tsx
+++
@@ -1,9 +0,0 @@
-import DrinkList from './DrinkList';
-import React from 'react';
-import products from '@/pages/productList.json'
-
-const MenuButton = () => {
-  return (
-    <button class='w-110 h-110 bg-Pink-50'></button>
-  )
-}
--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,19 +1,18 @@
-import productList from "./productList.json";
-import React, { useState } from "react";
-import Navigation from './components/Navigation.tsx';
+import React from "react";
+import { InputWithButton } from '@/components/Search';
+import App from '@/components/Plus.js';
 
 export default function Home() {
-  console.log(productList)
-  const [count, setCount] = useState(
-    productList.products.reduce((state, products) => {
-      state[products.id] = 0;
-      return state;
-    }, {})
-  );
   return (
     <>
-    <div className="w-[400px] h-[800px] bg-[#FFE4E6] rounded-[0px] absolute mx-auto">
-    <Navigation/>
+    <div className="w-[430px] h-[932px] bg-[#FFF] rounded-[0px] absolute mx-auto">
+      <p className='mt-10 ml-5 text-2xl font-bold text-[#000]'>Todo List</p>
+      <div className='mt-5 mx-5'>
+        <InputWithButton />
+      </div>
+      <div className="absolute bottom-0 right-0 h-10 w-10 bg-[#0F172A] rounded-full flex items-center justify-center">
+        <App />
+      </div>
     </div>
     </>
   )
